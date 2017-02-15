d3a980e2006ac69feecc279c4fab97fe4527eb61
Jonathan Mezach <jonathanm@infosupport.com>
2017-02-15 21:55:47 +0100

    Setup script for merging a single file

--- mkvmerge-subs.sh
+++ mkvmerge-subs.sh
@@ -4,4 +4,25 @@ die () {
     exit 1
 }
 
+# Validate arguments
 [ "$#" -eq 1 ] || die "Usage: $0 <path-to-srt>"
+[ -e "$1" ] || die "File $1 does not exist."
+
+# Find the associated MKV file and make sure it exists
+fullfile="$1"
+srtfile="${fullfile##*/}"
+mkvfile="${srtfile%.*}.mkv"
+tmpfile="${srtfile%.*}-subs.mkv"
+path="${fullfile%/*}"
+[ -e "$path/$mkvfile" ] || die "Video file $mkvfile does not exist."
+
+# Make sure that docker is installed
+docker -v >/dev/null 2>&1 || die "Docker is not installed"
+
+# Run a docker container to mkvmerge the subtitles in
+docker run --name "mkvmerge-subs" -v "$path":/source -w /source -it moul/mkvtoolnix mkvmerge -o /source/"$tmpfile" "$mkvfile" "$srtfile"
+docker rm "mkvmerge-subs"
+
+# Remove the original mkvfile and replace it with the merged file
+rm "$path/$mkvfile"
+mv "$path/$tmpfile" "$path/$mkvfile"
